734de4f1bb4c011524f7102e219fdb08c48cdb38
Michael Howard <michael.howard.pha@icloud.com>
2019-04-10 12:00:23 -0500

    PHP example

---
+++ gitACP.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+: '
+if [ $# -ne 1 ] || [[ ! " "$1" " == *" "'$1'" "* ]]
+    then
+        echo ERROR, need comment
+        exit 1
+    else
+'
+        git add .
+
+        # git commit -m $1
+
+        # git push origin master
+        
+        echo "FILE(s) PUSHED"
